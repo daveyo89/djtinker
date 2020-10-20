d914468d4b9cde790e9d3c760ec1424a369d8900
daveyo89 <j.tothdavid@gmail.com>
2020-10-21 00:41:12 +0200

    no comment

---
+++ blog/models.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from tinymce import models as tinymce_models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    slug = models.CharField(max_length=60, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save()
+
+    def get_absolute_url(self, *args, **kwargs):
+        return reverse('category', kwargs={'slug': self.slug})
+
+
+class Post(models.Model):
+    statuses = [
+        ('D', 'Draft'),
+        ('P', 'Publish')
+    ]
+    title = models.CharField(max_length=120)
+    slug = models.CharField(max_length=60, unique=True, blank=True)
+    # content = models.TextField()
+    content = tinymce_models.HTMLField()
+    status = models.CharField(max_length=1, choices=statuses)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog/post', default="post-default.jpg", blank=True)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save()
+
+    def get_absolute_url(self, *args, **kwargs):
+        return reverse('post-update', kwargs={'slug': self.slug})
+
+    def html_to_text(self, *args, **kwargs):
+        soup = BeautifulSoup(self.content, features="html.parser")
+        text = soup.get_text()
+        return text
---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from blog.views import SearchView, CategoryView, ContactView, PostListView, PostDetailView, PostCreateView, \
+    PostUpdateView, PostDeleteView
+
+urlpatterns = [
+    path('search', SearchView.as_view(), name="search"),
+    path('category/<slug:slug>', CategoryView.as_view(), name="category"),
+
+    path('', PostListView.as_view(), name="index"),
+    path('posts/<slug:slug>', PostDetailView.as_view(), name="post-detail"),
+
+    path('posts', PostCreateView.as_view(), name="post-create"),
+    path('posts/<slug:slug>/update', PostUpdateView.as_view(), name="post-update"),
+    path('posts/<slug:slug>/delete', PostDeleteView.as_view(), name="post-delete"),
+]
