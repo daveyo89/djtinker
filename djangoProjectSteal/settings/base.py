5ee4e48717f7822a017bb8d4f027cb5226c397cd
daveyo89 <j.tothdavid@gmail.com>
2020-10-24 16:15:39 +0200

    statics

--- djangoProjectSteal/settings/base.py
+++ djangoProjectSteal/settings/base.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'djangoProjectSteal.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -120,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
