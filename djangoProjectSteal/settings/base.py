315cfb22aa616c9654588d304550fa8a298ec616
daveyo89 <j.tothdavid@gmail.com>
2020-10-24 16:30:11 +0200

    asdf

--- djangoProjectSteal/settings/base.py
+++ djangoProjectSteal/settings/base.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
-MEDIA_URL = '/static/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Login/Logout Redirect urls
